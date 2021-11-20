bfb008fc9f10e1efc20ab3217c6c325dc7f01fa2
DRSS Enterprise <drss.enterprise@gmail.com>
2021-11-20 10:30:24 -0500

    Created a teleop so holden can have speed Controls

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CompTeleOp.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.controls.Controls;
+import org.firstinspires.ftc.teamcode.subsystems.robot.CompRobot;
+
+@TeleOp(name = "Comp TeleOp", group = "Comp")
+public class CompTeleOp extends LinearOpMode{
+
+	private GamepadEx gamepad1ex;
+	private GamepadEx gamepad2ex;
+	Controls controls;
+
+	CompRobot robot;
+
+	double speedOverride = 1;
+
+	@Override
+	public void runOpMode() throws InterruptedException{
+		gamepad1ex = new GamepadEx(gamepad1);
+		gamepad2ex = new GamepadEx(gamepad2);
+		controls = new Controls(gamepad1ex, gamepad2ex);
+
+		robot = new CompRobot(hardwareMap, telemetry);
+
+		waitForStart();
+
+		while(!isStopRequested()) {
+
+			/*Speed Controls*/
+			if (gamepad1ex.getButton(GamepadKeys.Button.X)) {
+				speedOverride = .25;
+			}
+			if (gamepad1ex.getButton(GamepadKeys.Button.Y)) {
+				speedOverride = 1;
+			}
+			if (controls.speedTrigger.isDown()) {
+				speedOverride = 0.75;
+			}
+			else if (controls.speedTrigger.wasJustReleased()) {
+				speedOverride = 1;
+			}
+
+			/*Drivetrain Control*/
+			robot.drive.setWeightedDrivePower(
+					new Pose2d(
+							-gamepad1.left_stick_y * speedOverride,
+							-gamepad1.left_stick_x * speedOverride,
+							-gamepad1.right_stick_x * speedOverride
+					)
+			);
+			robot.drive.update();
+			Pose2d poseEstimate = robot.drive.getPoseEstimate();
+
+			/*Telemetry*/
+			telemetry.addData("x", poseEstimate.getX());
+			telemetry.addData("y", poseEstimate.getY());
+			telemetry.addData("heading", poseEstimate.getHeading());
+			telemetry.update();
+
+			/*End of loop updates*/
+			gamepad1ex.readButtons();
+			gamepad2ex.readButtons();
+		}
+	}
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/controls/Controls.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.subsystems.controls;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Controls{
+
+	public TriggerReader speedTrigger;
+
+	public Controls(GamepadEx gamepad1ex, GamepadEx gamepad2ex) {
+
+		speedTrigger = new TriggerReader(
+				gamepad1ex, GamepadKeys.Trigger.RIGHT_TRIGGER
+		);
+
+	}
+}
