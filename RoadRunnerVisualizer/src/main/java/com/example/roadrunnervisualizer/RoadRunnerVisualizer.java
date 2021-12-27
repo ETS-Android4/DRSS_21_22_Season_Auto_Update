dfeae3c71f211872feb876a343fb01f165a860cf
gagne-3 <gagne.3@daytonstemschool.org>
2021-12-27 17:21:14 -0500

    messing around with roadrunner plotting

--- RoadRunnerVisualizer/src/main/java/com/example/roadrunnervisualizer/RoadRunnerVisualizer.java
+++ RoadRunnerVisualizer/src/main/java/com/example/roadrunnervisualizer/RoadRunnerVisualizer.java
@@ -1,6 +1,7 @@
 package com.example.roadrunnervisualizer;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -13,16 +14,50 @@ public class RoadRunnerVisualizer {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(52.4829, 52.4829, Math.toRadians(273.36816), Math.toRadians(273.36816), 12)
+                .setDimensions(13,17)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -61.5, Math.toRadians(90)))
+                                .splineToConstantHeading(new Vector2d(15,-50), Math.toRadians(90))
+                                .splineToConstantHeading(new Vector2d(15,-40), Math.toRadians(90))
+                                .splineToConstantHeading(new Vector2d(-11.5,-42), Math.toRadians(90))
+                                .waitSeconds(1)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(8,-64), Math.toRadians(0))
+                                .back(40)
+                                .setReversed(false)
+                                .waitSeconds(1)
+                                .forward(40)
+                                .splineTo(new Vector2d(-11.5,-42), Math.toRadians(90))
+                                .waitSeconds(1)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(8,-64), Math.toRadians(0))
+                                .back(40)
+                                .setReversed(false)
+                                .waitSeconds(1)
+                                .forward(40)
+                                .splineTo(new Vector2d(-11.5,-42), Math.toRadians(90))
+                                .waitSeconds(1)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(8,-64), Math.toRadians(0))
+                                .back(40)
+                                .setReversed(false)
+                                .waitSeconds(1)
+                                .forward(40)
+                                .splineTo(new Vector2d(-11.5,-42), Math.toRadians(90))
+                                .waitSeconds(1)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(8,-64), Math.toRadians(0))
+                                .back(40)
+                                .setReversed(false)
+                                .waitSeconds(1)
+                                .splineToConstantHeading(new Vector2d(38,-42), Math.toRadians(180))
+                                /*.turn(Math.toRadians(90))
                                 .forward(30)
                                 .turn(Math.toRadians(90))
                                 .forward(30)
                                 .turn(Math.toRadians(90))
                                 .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                                .turn(Math.toRadians(90))*/
                                 .build()
                 );
 
